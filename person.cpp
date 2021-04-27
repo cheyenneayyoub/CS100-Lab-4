b120bd9df0a42f445dade1dd9df215e037abf76b
cheyenneayyoub <cayyo001@ucr.edu>
2021-04-27 01:17:17 -0700

    7 errors total

--- person.cpp
+++ person.cpp
@@ -6,7 +6,7 @@ using std::cout;
 using std::endl;
 
 Person::Person(const char *name_, Person* father_, Person* mother_){
-    name = new char[strlen(name_)];
+    name = new char[strlen(name_) + 1];
     strcpy(name, name_);
     father = father_;
     mother = mother_;
@@ -16,7 +16,8 @@ Person::Person(const char *name_, Person* father_, Person* mother_){
 }
 
 Person::~Person(){
-    delete children;
+    delete[] children;
+    delete[] name;
 }
 
 void Person::addChild(Person *newChild){
@@ -52,31 +53,34 @@ void Person::printLineage(char dir, int level){
             father->printLineage(dir, level + 1);
         }
     }
+	delete[] temp;
 }
 
 /* helper function to compute the lineage
-* if level = 0 then returns the empty string
-* if level >= 1 then returns ("great ")^(level - 1) + "grand "
-*/
+ * * if level = 0 then returns the empty string
+ * * if level >= 1 then returns ("great ")^(level - 1) + "grand "
+ * */
 char* Person::compute_relation(int level){
-    if(level == 0) return strcpy(new char[1], "");
+    if(level == 0) return ""; 
 
-    char *temp = strcpy(new char[strlen("grand ") + 1], "grand ");;
+    char *temp = strcpy(new char[strlen("grand ") + 1], "grand ");
     
     for(int i = 2; i <= level; i++){
         char *temp2 = new char[strlen("great ") + strlen(temp) + 1];
         strcat(strcpy(temp2, "great "), temp);
-        temp = temp2;
+        delete[] temp;
+	temp = temp2;
     }
     return temp;
 }
 
 /* non-member function which doubles the size of t
- * NOTE: t's type will be a pointer to an array of pointers
- */
+ *  * NOTE: t's type will be a pointer to an array of pointers
+ *   */
 void expand(Person ***t, int *MAX){
   Person **temp = new Person*[2 * *MAX];
   memcpy(temp, *t, *MAX * sizeof(**t));
   *MAX *= 2;
-  *t = temp;
+  delete[] *t;
+  *t = temp; 
 }
